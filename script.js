c50a40245df08f07ff279b2dae005efa3fb876cd
OviyaR2021 <oviyaraja10@gmail.com>
2021-05-07 17:55:06 -0700

    Update

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 alert("Hello!");
+let canvasElement = document.getElementById("canvas");
+let ctx = canvasElement.getContext("2d");
+canvasElement.width = 300;
+canvasElement.height = 1000;
+
+let makeCanvas = function() {
+    ctx.fillStyle = "pink"
+}
