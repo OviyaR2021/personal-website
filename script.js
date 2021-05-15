8fad2e3b7cc04dc2062d74e98ff38dc127583b07
OviyaR2021 <oviyaraja10@gmail.com>
2021-05-14 17:34:49 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,5 +5,13 @@ canvasElement.width = 300;
 canvasElement.height = 1000;
 
 let makeCanvas = function() {
-    ctx.fillStyle = "pink"
+    ctx.fillStyle = "white";
+    ctx.strokeStyle = "black";
+    ctx.lineWidth = 2;
+    for (let row = 0; row < height; row++) {
+        for (let column = 0; column < width; column++) {
+            ctx.fillRect(column * cellWidth, row * cellHeight, cellWidth, cellHeight);
+            ctx.strokeRect(column * cellWidth, row * cellHeight, cellWidth, cellHeight);
+            }
+    }
 }
