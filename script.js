91e2639d9485655288f3de92ca8a7949a4114644
OviyaR2021 <oviyaraja10@gmail.com>
2021-05-14 17:51:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 alert("Hello!");
 let canvasElement = document.getElementById("canvas");
 let ctx = canvasElement.getContext("2d");
+let cellSize = 10;
 
 canvasElement.width = Window.innerWidth;
 canvasElement.height = Window.innerHeight;
@@ -14,8 +15,7 @@ let makeCanvas = function() {
     ctx.lineWidth = 2;
     for (let row = 0; row < numberOfRows; row++) {
         for (let column = 0; column < numberOfColumns; column++) {
-            ctx.fillRect(column * cellWidth, row * cellHeight, cellWidth, cellHeight);
-            ctx.strokeRect(column * cellWidth, row * cellHeight, cellWidth, cellHeight);
+            ctx.strokeRect(column * cellSize, row * cellSize, cellSize);
             }
     }
 }
