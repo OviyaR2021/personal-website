67be6d63cde9f77fcb1c6e0e2deb3457b9b9365f
OviyaR2021 <oviyaraja10@gmail.com>
2021-05-14 17:41:30 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 alert("Hello!");
 let canvasElement = document.getElementById("canvas");
 let ctx = canvasElement.getContext("2d");
-canvasElement.width = 300;
-canvasElement.height = 1000;
+canvasElement.width = Window.innerWidth;
+canvasElement.height = Window.innerHeight;
 
 let makeCanvas = function() {
     ctx.fillStyle = "white";
     ctx.strokeStyle = "black";
     ctx.lineWidth = 2;
-    for (let row = 0; row < height; row++) {
-        for (let column = 0; column < width; column++) {
+    for (let row = 0; row < numberOfRows; row++) {
+        for (let column = 0; column < numberOfColumns; column++) {
             ctx.fillRect(column * cellWidth, row * cellHeight, cellWidth, cellHeight);
             ctx.strokeRect(column * cellWidth, row * cellHeight, cellWidth, cellHeight);
             }
     }
 }
+
+makeCanvas();
